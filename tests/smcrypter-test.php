667121de27caea8d961fe29b14855fdccc4b30ce
Léo Castro <leonardo_carvalho@outlook.com>
2016-10-16 18:09:38 -0200

    Integrity Tests

--- tests/smcrypter-test.php
+++ tests/smcrypter-test.php
@@ -1,15 +1,23 @@
 <?php
 
 include "../src/SMCrypter.php";
-
-
 $msgCrypt = new SMCrypter();
 
-$key = $msgCrypt->keyGenerator();
-var_dump($key);
-
-$en = $msgCrypt->encode($key, 126);
-var_dump($en);
+$originalValue = 65; //A
+echo 'Original Value: '.$originalValue;
 
 echo "<br>";
-var_dump($msgCrypt->decode($key, $en));
+
+$key = $msgCrypt->keyGenerator();
+//$key = 3000000;
+//$key = 'VE16Z2pNPT1RTzVn';
+echo 'Generated Key: '.$key;
+
+echo "<br>";
+
+$en = $msgCrypt->encode($key, 65);
+echo 'Encrypted Value: '.$en;
+
+echo "<br>";
+
+echo 'Decrypted Value: '.$msgCrypt->decode($key, $en);
